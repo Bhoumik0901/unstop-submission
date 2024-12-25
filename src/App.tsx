aefe9141c85718dac163ea2239e5d331c1b455ee
Bhoumik <Bhoumikchopra12@gmail.com>
2024-12-25 17:37:18 +0530

    added redirects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes, useNavigate } from "react-router-dom"
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
 import Login from "./Components/Login"
 import Profile from "./Components/Profile"
 import { useEffect} from "react"
 
 const App = () => {
   const navigate = useNavigate();
+
   useEffect(() => {
     const user = localStorage.getItem('user');
     if (user) {
@@ -13,13 +14,15 @@ const App = () => {
     } else {
       navigate('/login'); // Navigate to home if user doesn't exist
     }
-  }, []);
+  }, [navigate]);
 
   return (
     <div className="w- h-screen">
       <Routes>
+        
         <Route element={<Login />} path="/login" />
         <Route element={<Profile />} path="/home" />
+        <Route path="*" element={<Navigate to="/login" />} />
 
       </Routes>
 
